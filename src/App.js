de5d71a91f300f55c71041b44508c394e37c2668
Vinnie King <vinnie.king@tallwave.com>
2018-02-20 22:00:17 -0700

    put in the really ugly one

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Scatter from './scatter-2';
+import BadScatter from './scatter';
 import Line from './line';
 import logo from './logo.svg';
 import Pie from './pie';
@@ -21,7 +22,7 @@ class App extends Component {
         <header className="App-header">
           <img src={'https://cdn.thealternativedaily.com/wp-content/uploads/2017/05/Just-one-coffee-drink-may-contain-50-grams-of-sugar.jpg'} className="App-logo" alt="logo" />
           <h1 className="App-title">Spending too much on coffee</h1>
-          <span onClick={() => { this.changeGraph('pie')}}>Pie   </span><span onClick={() => { this.changeGraph('line')}}>Line   </span><span onClick={() => { this.changeGraph('scatter')}}>Scatter</span>
+          <span onClick={() => { this.changeGraph('pie')}}>Pie   </span><span onClick={() => { this.changeGraph('line')}}>Line   </span><span onClick={() => { this.changeGraph('scatter')}}>Scatter   </span> <span onClick={() => { this.changeGraph('bad')}}> Really bad</span>
         </header>
       { renderGraph(this.state.graph)}
       </div>
@@ -37,6 +38,8 @@ const renderGraph = (state) => {
       return <Line />;
     case 'scatter':
       return <Scatter />
+    case 'bad':
+      return <BadScatter />
   }
 }
 export default App;
